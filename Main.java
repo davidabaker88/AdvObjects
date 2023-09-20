c833739987f995889f31e8e54c9cc346f6713c20
David Baker <dbaker@inghamisd.org>
2023-09-20 11:20:56 -0400

    Update Main.java wording for creating objects

--- Main.java
+++ Main.java
@@ -6,12 +6,12 @@ public class Main {
         //ask a classmate or instructor if you are unsure how the code works.
 
 
-        //generate a new player
+        //create a new player object
 
-        //main game loop that continues while the player is alive
-        //fill the () with a call to a method that checks if the player is alive
+        /*main game loop that continues while the player is alive
+        fill the () with a call to a method that checks if the player is alive*/
         while( ) {
-            //generate a new enemy( the level of the enemy should be the player's level)
+            //create a new enemy obect  (the level of the enemy should be the player's level)
 
             //battle loop that should continue while both the player and enemy are alive
             //Fill the () with a check to make sure the player is alive and(&&) the enemy is alive
@@ -24,8 +24,8 @@ public class Main {
 
             }
         }
-        //once the player is dead print out the player's level and how much xp they had.
-        //use the following format: Level: 2 XP: 5
+        /*once the player is dead print out the player's level and how much xp they had.
+        use the following format: Level: 2 XP: 5*/
 
     }
 }
